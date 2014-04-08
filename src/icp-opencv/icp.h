15d9b614f0534deb77feb1cdde3c24d204683bfd
abreheret <abreheret@gmail.com>
2014-04-08 09:21:58 +0200

    correction documentation

--- src/icp-opencv/icp.h
+++ src/icp-opencv/icp.h
@@ -12,19 +12,19 @@ extern "C" {
     
 // Compute icp with 2 set points (between ref points and new points)
 // inputs :
-//   - new_points      : set of new input points (source point clouds)
-//   - nb_point_new_pt : number of input new points
-//   - ref_points      : set of reference input points.
-//   - nb_point_ref_pt : number of input old points
-//   - criteria        : default is cvTermCriteria( CV_TERMCRIT_ITER, nb_max_iteration, stop_epsilon )
-//   - image           : optional (for draw nearest points )
+//   - new_points   : set of new input points (source point clouds)
+//   - nb_point_new : number of input new points
+//   - ref_points   : set of reference input points.
+//   - nb_point_ref : number of input old points
+//   - criteria     : default is cvTermCriteria( CV_TERMCRIT_ITER, nb_max_iteration, stop_epsilon )
+//   - image        : optional (for draw nearest points )
 // output 2 matrix :
 //   - r_out : rotation matrix, 2x2 with type = CV_32F
 //   - t_out : translation maxtrix 2x1 with type = CV_32F
 // return :
 //   - err : cumulative sum of distance points
-float icp(const CvPoint2D32f* new_points,int nb_point_new_pt,
-          const CvPoint2D32f* ref_points,int nb_point_ref_pt,
+float icp(const CvPoint2D32f* new_points,int nb_point_new,
+          const CvPoint2D32f* ref_points,int nb_point_ref,
           CvMat * r_out,CvMat *t_out,
           CvTermCriteria criteria, IplImage * image CV_DEFAULT(NULL)) ;
 
